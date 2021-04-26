d98418abe6342ebff7d7d18ae21c4e2aa16f0405
James-Oswald <jamesoswald111@gmail.com>
2021-04-26 03:34:56 -0400

    finished edit and load

--- Server/centralAuthority.js
+++ Server/centralAuthority.js
@@ -4,6 +4,17 @@
 
 let WebSocket = require("ws");
 
+class Session{
+    constructor(documentID, websocket){
+        this.id = documentID;
+        this.users = [websocket];
+        this.documentHistory = [];
+    }
+}
+
+//dictionary of sessions indexed by document ID
+let sessions = {};
+
 //=============== Application State =========================
 const wss = new WebSocket.Server({port: 8080}); //The websocket Server
 
@@ -14,31 +25,57 @@ function init(){
     wss.on('connection', onConnection);
 }
 
-function onConnection(websocket){
-    websocket.on('message', message=>onMessage(websocket, message));
-    websocket.on('close', (code,reason)=>onClose(websocket, code, reason));
-}
-
-function onMessage(websocket, message){
+function onConnection(websocket, request){
     try{
-        dispatch(websocket, message)
+        let connectionParams = new URL("http://meme.com" + request.url).searchParams;
+        let documentId = connectionParams.get("doc");
+        //let user = connectionParams.get("doc");
+        if(sessions[documentId] == undefined)   //if the user has yet to connect
+            sessions[documentId] = new Session(documentId, websocket)
+        else{ //someone is already connected, we add ourselves to the session
+            sessions[documentId].users.push(websocket);
+            let loadMessage = {
+                id: 0,
+                type: "load",
+                edits: sessions[documentId].documentHistory
+            }
+            websocket.send(JSON.stringify(loadMessage));
+        }
+        let thisSession = sessions[documentId];
+        websocket.on('message', message=>onMessage(websocket, message, thisSession));
+        websocket.on('close', (code,reason)=>onClose(thisSession));
+        
+        console.log("connection sucsessful");
     }catch(e){
-        console.log(e.message);
+        console.log(e);
     }
 }
 
-function dispatch(websocket, message){
+function onMessage(websocket, message, session){
+    let messageObject = JSON.parse(message);
     try{
-        let obj = JSON.parse(message);
-        websocket.send(JSON.stringify({"retMsg":obj.enc}));
+        dispatch(messageObject, session);
     }catch(e){
-        console.log(e.message);
-        websocket.send(JSON.stringify({"retMsg":e.message}));
+        console.log(e);
+        websocket.send(JSON.stringify({"id": messageObject.id, "error":e.message}));
     }
 }
 
-function onClose(){
-    
+function dispatch(messageObject, session){
+    console.log(messageObject);
+    switch(messageObject.type){
+        case "edit":   //just forwards it to all connections
+            session.documentHistory.push(messageObject.msg)
+            for(let user of session.users)
+                user.send(JSON.stringify(messageObject));
+            break;
+        default:
+            throw new Error("Invalid message type");
+    }
+}
+
+function onClose(thisSession){
+
 }
 
 
--- WebClient/index.js
+++ WebClient/index.js
@@ -1,23 +1,90 @@
 
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('ws://localhost:8080?doc="a"');
+let messageTable = {}
+
+//we generate this in place of a real user system
+userId = Math.random().toString(36).substring(7);
 
 socket.addEventListener('open', function(event){
-    console.log("Server Connect")
+    console.log("Server Connect");
 });
 
 socket.addEventListener('message', function (event) {
-    console.log(event.data)
-    decObj = atob(JSON.parse(event.data).retMsg);
-    document.getElementById("out").innerHTML += decObj + "<br/>";
+
+    let messageObject = JSON.parse(event.data);
+    console.log(messageObject);
+    if(messageTable[messageObject.id] != undefined)
+        delete messageTable[messageObject.id];
+    else{   //This is a request from another user
+        let editArea = document.getElementById("editArea");
+        switch(messageObject.type){
+            case "edit":
+                let editObject = JSON.parse(CryptoJS.AES.decrypt(messageObject.msg, "password").toString(CryptoJS.enc.Utf8));
+                console.log(editObject);
+                if(editObject.act == 1)
+                    editArea.value = editArea.value.substring(0, editObject.pos) + editObject.val + editArea.value.substring(editObject.pos, editArea.value.length);
+                else if(editObject.act == 0)
+                    editArea.value = editArea.value.substring(0, editObject.pos) + editArea.value.substring(editObject.pos + 1, editArea.value.length);
+                else
+                    console.log("invalid edit action");
+                editText = editArea.value;
+                break;
+            case "load":
+                for(let encryptedEdit of messageObject.edits){
+                    let editObject = JSON.parse(CryptoJS.AES.decrypt(encryptedEdit, "password").toString(CryptoJS.enc.Utf8));
+                    console.log(editObject);
+                    if(editObject.act == 1)
+                        editArea.value = editArea.value.substring(0, editObject.pos) + editObject.val + editArea.value.substring(editObject.pos, editArea.value.length);
+                    else if(editObject.act == 0)
+                        editArea.value = editArea.value.substring(0, editObject.pos) + editArea.value.substring(editObject.pos + 1, editArea.value.length);
+                    else
+                        console.log("invalid edit action");
+                }
+                editText = editArea.value;
+                break;
+            default:
+                console.log("invalid message type recived")
+        }
+    }
 });
 
-let counter = 0;
-let counter2 = 0;
-
-function send(){
-    counter++;
-    counter2+=10;
-    obj = {"a":counter, "b":counter2};
-    encObj = btoa(JSON.stringify(obj));
-    socket.send(JSON.stringify({"enc":encObj}));
+editText = "";
+messageNumber = 0;
+function onEdit(element){
+    let editType = null;
+    let editValue = null;
+    let editPosition = null;
+    if(element.selectionEnd != element.selectionStart){ //we force no selection
+        element.value = editText;
+        return;
+    }
+    //var lastInput = getInputedString(editText, element.value, element.selectionEnd);
+    if(editText.length > element.value.length){
+        editType = 0; //delete
+        editPosition = element.selectionEnd;
+        editValue = 1; 
+    }else{
+        editType = 1; //add
+        editPosition = element.selectionEnd - 1;
+        editValue = element.value[element.selectionEnd - 1];
+    }
+    editText = element.value;
+    editMessage = {
+        user: userId,
+        time: Date.now(),
+        act: editType,
+        pos: editPosition,
+        val: editValue
+    };
+    console.log(editMessage);
+    let encryptedEdit = CryptoJS.AES.encrypt(JSON.stringify(editMessage), "password").toString();
+    messageId = userId + messageNumber++;
+    message = {
+        id: messageId,
+        type: "edit",
+        msg: encryptedEdit
+    };
+    socket.send(JSON.stringify(message));
+    messageTable[messageId] = "";
+    //send input
 }
